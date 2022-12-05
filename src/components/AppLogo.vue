f6abd50dfd31ef35cb0c81f4305a3775b4d9205c
SimoVisc <simonaviscardivs@gmail.com>
2022-12-05 12:00:25 +0100

    Aggiunge logo

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+    name: "AppLogo",
+   }
+</script>
+
+<template>
+  <img src="../assets/img/dark-logo.png" alt="logo">
+</template>
+
+<style lang="scss" scoped>
+
+</style>
