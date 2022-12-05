2069f204d51c54bb334d5b8a8c5bcbe65f1e1f76
SimoVisc <simonaviscardivs@gmail.com>
2022-12-05 14:06:18 +0100

    Aggiunge hero

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,36 +10,56 @@ export default {
 </script>
 
 <template>
+  <header>
   <div class="container">
     <AppLogo/>
-    <div class="nav-bar">
-      <ul>
-        <li>Home <i class="fa-solid fa-chevron-down"></i> </li>
-        <li>Pages <i class="fa-solid fa-chevron-down"></i> </li>
-        <li>Courses <i class="fa-solid fa-chevron-down"></i> </li>
-        <li>Features <i class="fa-solid fa-chevron-down"></i> </li>
-        <li>Blog <i class="fa-solid fa-chevron-down"></i> </li>
-        <li> Shop <i class="fa-solid fa-chevron-down"> <i class="fa-solid fa-cart-shopping"></i></i> </li>
-        <li><i class="fa-solid fa-circle-user"></i></li>
-      </ul>
-   </div>
-   <div class="search-bar">
-     <input type="text" placeholder="Search...">
-     <button><i class="fa-solid fa-magnifying-glass"></i></button>
-   </div>
- 
-
+    <div class="bars">
+      <div class="nav-bar">
+        <ul>
+          <li>Home <i class="fa-solid fa-chevron-down"></i> </li>
+          <li>Pages <i class="fa-solid fa-chevron-down"></i> </li>
+          <li>Courses <i class="fa-solid fa-chevron-down"></i> </li>
+          <li>Features <i class="fa-solid fa-chevron-down"></i> </li>
+          <li>Blog <i class="fa-solid fa-chevron-down"></i> </li>
+          <li> Shop <i class="fa-solid fa-chevron-down"> <i class="fa-solid fa-cart-shopping big"></i></i> </li>
+          <li><i class="fa-solid fa-circle-user big"></i></li>
+        </ul>
+     </div>
+     <div class="search-bar">
+       <input type="text" placeholder="Search...">
+       <button><i class="fa-solid fa-magnifying-glass"></i></button>
+     </div>
+    </div>
   </div>
+    <div class="hero container">
+      <div class="text-container">
+        <h1>Complete Your Dreams in JavaS Programming</h1>
+        <p>Learn any coding program in distance and for a reasonable fee. you don't have to struggle alone, you've got our assistance and help</p>
+        <button>Download from E-book</button>
+      </div>
+      <div class="img-container">
+        <img src="../assets/img/home-5-hero-image.png" alt="">
+      </div>
+    </div>
+  
+</header>
 </template>
 
 <style lang="scss" >
+header{
+  background-image: url(../assets/img/jumbo-overlay.svg);
+  background-color: #e9e6e3;
 .container{
-  
   display:flex;
   justify-content: space-between;
-  align-items:center;
+ 
+  .bars{
+    display:flex;
+    align-items:center;
+  }
   ul{
     display:flex;
+    padding-left: 10px;
 
     li{
       list-style: none;
@@ -48,21 +68,45 @@ export default {
       i{
         font-size: 10px;
       }
+      .big{
+        font-size: 16px;
+        padding-right:5px
+      }
     }
   }
   .search-bar{
-    text-align: end;
+  display: flex;
+    justify-content: end;
+
     input{
       border:none;
       padding:5px;
-      width:40%
+      width:30%
     }
+    
     button{
       color: #20ad96;
       background-color: #fff;
       border:none;
       padding:5px;
     }
+  }
+}
+.hero{
+  .text-container{
+    width:50%;
+    padding:80px;
+    p{
+      padding:15px 0;
+    }
+  }
+  .img-container{
+    width:50%;
+   padding-right:20px;
+   img{
+    width:100%
+   }
+  }
   }
 }
 </style>
