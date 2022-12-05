d7a3fed5aa601611b7af6008f315844959ccd50f
SimoVisc <simonaviscardivs@gmail.com>
2022-12-05 15:06:35 +0100

    Aggiunge AppSection1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,81 +1,26 @@
 <script>
+import AppSection1 from './AppSection1.vue';
 export default {
   name: 'AppMain',
+  components:{
+    AppSection1
+  }
 }
 </script>
 
 <template>
-  <main class="container">
-     <article class="start-learning" >
-        <small>START LEARNING CODING LANGUAGES</small>
-        <div class="title">
-            <h1>Build Your Dream</h1>
-            <h1 class="green-color">TODAY</h1>
-        </div>
-        <div class="sections-container">
-            <section>
-                <img src="../assets/img/home-5-image-box-01.png" alt="">
-                <h4>Idea Discussion</h4>
-                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
-                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
-            </section>
-            <section>
-                <img class="img-big" src="../assets/img/home-5-image-box-02.png" alt="">
-                <h4>Idea Discussion</h4>
-                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
-                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
-            </section>
-            <section>
-                <img src="../assets/img/home-5-image-box-03.png" alt="">
-                <h4>Idea Discussion</h4>
-                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
-                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
-            </section>
-            <section>
-                <img src="../assets/img/home-5-image-box-04.png" alt="">
-                <h4>Idea Discussion</h4>
-                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
-                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
-            </section>
-        </div>
- </article>
+  <main>
+    <AppSection1/>
+ <section class="dream with MaxCoach">
+   <img src="../assets/img/about-us-03-image-01.png" alt="">
+    <div class="title">
+        <small>DREAM WITH <strong>MAXCOACH</strong></small>
+        <h1>Construct A <span class="green-color">Stunning</span> Career Prospective </h1>
+    </div>
+ </section>
 </main>
 </template>
 
 <style lang="scss">
-.container{
-    text-align: center;
-    padding-top:40px;
-    .title{
-      display: flex;
-      justify-content: center;
-      margin:20px 0;
-      .green-color{
-         color: #62c5b5
-        }
-    }
-    .sections-container{
-        display: flex;
-        align-items: center;
-        padding: 80px 0;
-       section{
-        padding:10px;
-        img{
-            width:100px;
-            padding: 10px 0;
-            margin: 30px 0
-        }
-        .img-big{
-            margin: 17px 0
-        }
-        p{
-            padding:20px 0
-        }
-        a{
-          color:gray;
-        }
-       }
-        
-    }
-}
+
 </style>
---
+++ src/components/AppSection1.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+  name: 'AppSection1',
+}
+</script>
+
+<template>
+  <section class="container" >
+        <small>START LEARNING CODING LANGUAGES</small>
+        <div class="title">
+            <h1>Build Your Dream</h1>
+            <h1 class="green-color">TODAY</h1>
+        </div>
+        <div class="articles-container">
+            <article>
+                <img src="../assets/img/home-5-image-box-01.png" alt="">
+                <h4>Idea Discussion</h4>
+                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
+                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
+            </article>
+            <article>
+                <img class="img-big" src="../assets/img/home-5-image-box-02.png" alt="">
+                <h4>Idea Discussion</h4>
+                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
+                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
+            </article>
+            <article>
+                <img src="../assets/img/home-5-image-box-03.png" alt="">
+                <h4>Idea Discussion</h4>
+                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
+                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
+            </article>
+            <article>
+                <img src="../assets/img/home-5-image-box-04.png" alt="">
+                <h4>Idea Discussion</h4>
+                <p>Get teamed up with the specialists who work and teach coding for years at famous universities</p>
+                <a href="#">start now <i class="fa-solid fa-arrow-right"></i></a>
+            </article>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" >
+.container{
+    text-align: center;
+    padding-top:40px;
+    .title{
+      display: flex;
+      justify-content: center;
+      margin:20px 0;
+      .green-color{
+         color: #62c5b5
+        }
+    }
+    .articles-container{
+        display: flex;
+        align-items: center;
+        padding: 80px 0;
+       section{
+        padding:10px;
+        img{
+            width:100px;
+            padding: 10px 0;
+            margin: 30px 0
+        }
+        .img-big{
+            margin: 17px 0
+        }
+        p{
+            padding:20px 0
+        }
+        a{
+          color:gray;
+        }
+       }
+        
+    }
+}
+</style>
