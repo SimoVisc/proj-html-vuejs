418584cda49517937a43667b5a8796dfed78e153
SimoVisc <simonaviscardivs@gmail.com>
2022-12-06 10:12:51 +0100

    Aggiunge AppsectionEvents

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,12 +2,23 @@
 import AppSectionToday from './AppSectionToday.vue';
 import AppSectionStunning from './AppSectionStunning.vue';
 import AppSectionCourses from './AppSectionCourses.vue';
+import AppSectionPace from './AppSectionPace.vue';
+import AppSectionMobile from './AppSectionMobile.vue';
+import AppSectionEvents from './AppSectionEvents.vue';
+import AppSectionLoveUs from './AppSectionLoveUs.vue';
+import AppSectionOnlineCertification from './AppSectionOnlineCertification.vue';
+
 export default {
   name: 'AppMain',
   components:{
     AppSectionToday,
     AppSectionStunning,
-    AppSectionCourses
+    AppSectionCourses,
+    AppSectionPace,
+    AppSectionMobile,
+    AppSectionEvents,
+    AppSectionLoveUs,
+    AppSectionOnlineCertification
 }
 }
 </script>
@@ -17,7 +28,11 @@ export default {
     <AppSectionToday/>
     <AppSectionStunning/>
     <AppSectionCourses/>
- 
+    <AppSectionPace/>
+    <AppSectionMobile/>
+    <AppSectionEvents/>
+    <AppSectionLoveUs/>
+    <AppSectionOnlineCertification/>
 </main>
 </template>
 
---
+++ src/components/AppSectionEvents.vue
@@ -0,0 +1,118 @@
+<script>
+export default {
+  name: 'AppSectionEvents',
+}
+</script>
+
+<template>
+    <section>
+        <div class="title">
+            <small>GET IN CONTACT NOW</small>
+            <h1>Upcoming<span class="green-color">Events</span></h1>
+        </div>
+        <div class="event-container">
+            <div class="event-info">
+            <small> <i class="fa-solid fa-location-dot"></i> Texas, US </small>
+            <h5>Storytellig Workshop</h5>
+            </div>
+            <div class="event-date">
+                <div class="green-color number"> 22</div>
+                <span>NOV</span>
+                <button>Get ticket</button>
+            </div>
+       </div>
+       <div class="event-container">
+            <div class="event-info">
+            <small> <i class="fa-solid fa-location-dot"></i> New York, US </small>
+            <h5>Painting Art Contest 2020</h5>
+            </div>
+            <div class="event-date">
+                <div class="green-color number"> 10</div>
+                <span>OCT</span>
+                <button>Get ticket</button>
+            </div>
+       </div>
+       <div class="event-container">
+            <div class="event-info">
+            <small> <i class="fa-solid fa-location-dot"></i> Hamburg, Germany </small>
+            <h5>International Art Fair 2020</h5>
+            </div>
+            <div class="event-date">
+                <div class="green-color number"> 23</div>
+                <span>NOV</span>
+                <button>Get ticket</button>
+            </div>
+       </div>
+       <div class="event-container">
+            <div class="event-info">
+            <small> <i class="fa-solid fa-location-dot"></i> Illinois, US </small>
+            <h5>Street Performance: Call for Artist</h5>
+            </div>
+            <div class="event-date">
+                <div class="green-color number"> 15</div>
+                <span>DEC</span>
+                <button>Get ticket</button>
+            </div>
+       </div>
+       <div class="event-container">
+            <div class="event-info">
+            <small> <i class="fa-solid fa-location-dot"></i>Illinois, US </small>
+            <h5>Consumer Food Safety Education Conference</h5>
+            </div>
+            <div class="event-date">
+                <div class="green-color number"> 22</div>
+                <span>JUL</span>
+                <button>Get ticket</button>
+            </div>
+       </div>
+       <div class="event-container">
+            <div class="event-info">
+            <small> <i class="fa-solid fa-location-dot"></i> Dubai </small>
+            <h5>How meditation improve your mental health</h5>
+            </div>
+            <div class="event-date">
+                <div class="green-color number"> 12</div>
+                <span>AUG</span>
+                <button>Get ticket</button>
+            </div>
+       </div>
+       <span>Exited about our event? <a href="#"> view all events </a></span>
+  </section>
+</template>
+  
+  <style lang="scss" scoped >
+  section{
+    
+   width:100%;
+   padding-top:40px;
+   display:flex;
+   justify-content:center;
+   flex-wrap: wrap;
+  
+   .title{
+    width:100%;
+    text-align: center;
+   }
+   .event-container{
+    width: 40%;
+    background-color: #f8f8f8 ;
+    display: flex;
+    margin: 20px;
+    padding:20px;
+
+    .event-info{
+        width:80%
+    }
+    .event-date{
+        width:20%;
+        .number{
+            font-size: 50px;
+
+        }
+    }
+
+   }
+
+  }
+  
+  </style>
