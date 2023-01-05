c2e6313d50396f2efd6987c7ca0a9dac8e24e1fd
SimoVisc <simonaviscardivs@gmail.com>
2023-01-05 19:27:09 +0100

    modifica grandezza e posizione immagini

--- src/components/AppSectionStunning.vue
+++ src/components/AppSectionStunning.vue
@@ -1,94 +1,112 @@
 <script>
-import AppAccordion from './AppAccordion.vue';
+import AppAccordion from "./AppAccordion.vue";
 export default {
-  name: 'AppSectionStunning',
-  components:{
-    AppAccordion
-  }
-}
+  name: "AppSectionStunning",
+  components: {
+    AppAccordion,
+  },
+  data() {
+    return {
+      data: [
+        {
+          number: "1.926",
+          name: "FINISHED SESSIONS",
+        },
+        {
+          number: "3.092+",
+          name: "ENROLLED LEARNERS",
+        },
+        {
+          number: "200",
+          name: "ONLINE INSTRUCTORS",
+        },
+        {
+          number: "100%",
+          name: "SATISFACTION RATE",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <section class="container">
     <div class="section-top">
-      <img class="img-1" src="../assets/img/underlay-shape-lilla.svg" alt="">
-      <img class="img-2" src="../assets/img/stunning-1.png" alt="">
+      <div class="img-container">
+        <img
+          class="img-1"
+          src="../assets/img/underlay-shape-lilla.svg"
+          alt=""
+        />
+        <img class="img-2" src="../assets/img/stunning-1.png" alt="" />
+      </div>
       <div class="text">
         <div class="title">
           <small>DREAM WITH <strong>MAXCOACH</strong></small>
-          <h1>Construct A <span class="green-color">Stunning</span> Career Prospective </h1>
+          <h1>
+            Construct A <span class="green-color">Stunning</span> Career
+            Prospective
+          </h1>
         </div>
         <AppAccordion />
-     </div>
-   </div>
-  </section> 
+      </div>
+    </div>
+  </section>
   <div class="bg-img">
-   <section class="section-bottom container">
-     <div class="number sessions">
-        <strong class="green-color">1.926</strong>
-        <small>FINISHED SESSIONS</small>
-     </div>
-     <div class="number learners">
-        <strong class="green-color">3.092+</strong>
-        <small>ENROLLED LEARNERS</small>
-     </div>
-     <div class="number instructors">
-        <strong class="green-color">200</strong>
-        <small>ONLINE INSTRUCTORS</small>
+    <section class="section-bottom container">
+      <div class="data" v-for="item in data">
+        <strong class="green-color number">{{ item.number }}</strong>
+        <small>{{ item.name }}</small>
       </div>
-      <div class="number satisfaction-rate">
-        <strong class="green-color">100%</strong>
-        <small>SATISFACTION RATE</small>
-      </div>
-   </section>
+    </section>
   </div>
 </template>
 
-<style lang="scss" scoped >
-
-.section-top{
-  display:flex;
-  .img-1{
-    position:relative;
-    width: 30%;
-    top:auto;
-    left:auto;
-  }
-  .img-2{
-    position:absolute;
-    top:auto;
-    left:auto;
-  }
-  .text{
-   width:50%;
-   padding: 30px;
-   margin-left: 20%;
-   text-align: start;
-   .title{
-     margin:20px ;
-   }
-  } 
-}
-.bg-img{   
-  background-color: var(--bg-primary-color);
-.section-bottom{
-  display:flex;
-  padding:20px;
-  margin-top: 15%;
-  .number{
-    width: 25%;
-    text-align: center;
-      
-    strong{
-      display: block;
-      font-size: 40px;
+<style lang="scss" scoped>
+.section-top {
+  display: flex;
+  .img-container {
+    width: 50%;
+    position: relative;
+    .img-1 {
+      height: 60%;
+      position: absolute;
+      top: 3rem;
+      right: 7rem;
     }
-  } 
+    .img-2 {
+      height: 75%;
+      position: relative;
+      top: auto;
+      left: 1.625rem;
+    }
+  }
+  .text {
+    width: 50%;
+    padding: 0 1.25rem;
+
+    .title {
+      padding-bottom: 3.125rem;
+      text-align: start;
+    }
+  }
 }
+.bg-img {
+  background-color: var(--bg-primary-color);
+  .section-bottom {
+    display: flex;
+    justify-content: space-between;
+    padding: 1.25rem;
+    margin-top: 15%;
+
+    .data {
+      text-align: center;
+    }
+    .number {
+      display: block;
+      font-size: 2.5rem;
+    }
+  }
 }
 </style>
-
-
-
-
-
